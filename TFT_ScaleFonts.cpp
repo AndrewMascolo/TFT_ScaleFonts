474438584b7ceb5448146497ffc788561c2d401d
AndrewMascolo <Adeathsk8r@comcast.net>
2016-05-21 20:47:55 -0400

    bug fix with landscape and portrait modes

--- TFT_ScaleFonts.cpp
+++ TFT_ScaleFonts.cpp
@@ -1,90 +1,75 @@
 #include "TFT_ScaleFonts.h"
 
-/*
-The MIT License (MIT)
-
-Copyright (c) 2016 Andrew Mascolo Jr
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
+void TFT_ScaleFonts::TextBounds(int x, int y, int x2, int y2)
+{
+  Bound_X = x;
+  Bound_Y = y;
+  Bound_X2 = x2;
+  Bound_Y2 = y2;
+}
 
 void TFT_ScaleFonts::printChar(byte c, int x, int y, int S)
 {
+  if (S < 1) S = 1;
+
+  if (S == 1)
+  {
+    _Disp->printChar(c, x, y);
+    return;
+  }
+
   word temp;
   unsigned char ch;
 
   cbi(_Disp->P_CS, _Disp->B_CS);
 
-  if (!_Disp->_transparent)
+  temp = ((c - _Disp->cfont.offset) * ((_Disp->cfont.x_size / 8) * _Disp->cfont.y_size)) + 4;
+
+  for (int Y = 0; Y < ((_Disp->cfont.x_size / 8) * _Disp->cfont.y_size)*S; Y += (_Disp->cfont.x_size / 8) * S)
   {
-    temp = ((c - _Disp->cfont.offset) * ((_Disp->cfont.x_size / 8) * _Disp->cfont.y_size)) + 4;
-
-    for (int Y = 0; Y < ((_Disp->cfont.x_size / 8) * _Disp->cfont.y_size)*S; Y += (_Disp->cfont.x_size / 8) * S)
+    for (byte s = 0; s < S; s++)
     {
-      for (byte s = 0; s < S; s++)
-      {
-        _Disp->setXY(x, y + s + (Y / (_Disp->cfont.x_size / 8)), x + _Disp->cfont.x_size * S - 1, y + s + (Y / (_Disp->cfont.x_size / 8)));
+      _Disp->setXY(x, y + s + (Y / (_Disp->cfont.x_size / 8)), x + _Disp->cfont.x_size * S - 1, y + s + (Y / (_Disp->cfont.x_size / 8)));
 
-        for (int xx = (_Disp->cfont.x_size / 8) - 1; xx >= 0; xx--)
+      for (int xx = (_Disp->cfont.x_size / 8) - 1; xx >= 0; xx--)
+      {
+        if (_Disp->orient == LANDSCAPE)
         {
           ch = pgm_read_byte(&_Disp->cfont.font[temp + xx]);
-          for (byte i = 0; i < 8 * S; i++)
+          for (byte i = 0; i < (8 * S) ; i++)
           {
-            if ((ch & (1 << i / S)) != 0)
+            if ((ch & (1 << (i / S))) != 0)
             {
               _Disp->setPixel((_Disp->fch << 8) | _Disp->fcl);
             }
             else
             {
-              _Disp->setPixel((_Disp->bch << 8) | _Disp->bcl);
+              if (!_Disp->_transparent)
+                _Disp->setPixel((_Disp->bch << 8) | _Disp->bcl);
             }
           }
         }
-      }
-      temp += (_Disp->cfont.x_size / 8);
-    }
-  }
-  else
-  {
-    temp = ((c - _Disp->cfont.offset) * ((_Disp->cfont.x_size / 8) * _Disp->cfont.y_size)) + 4;
-
-    for (int Y = 0; Y < ((_Disp->cfont.x_size / 8) * _Disp->cfont.y_size)*S; Y += (_Disp->cfont.x_size / 8) * S)
-    {
-      for (byte s = 0; s < S; s++)
-      {
-        _Disp->setXY(x, y + s + (Y / (_Disp->cfont.x_size / 8)), x + _Disp->cfont.x_size * S - 1, y + s + (Y / (_Disp->cfont.x_size / 8)));
-
-        for (int xx = (_Disp->cfont.x_size / 8) - 1; xx >= 0; xx--)
+        else
         {
-          ch = pgm_read_byte(&_Disp->cfont.font[temp + xx]);
-          for (byte i = 0; i < 8 * S; i++)
+          ch = pgm_read_byte(&_Disp->cfont.font[temp + 1 - xx]);
+          for (byte i = (8 * S) - 1; i > 0 ; i--)
           {
-            if ((ch & (1 << i / S)) != 0)
+            if ((ch & (1 << (i / S))) != 0)
             {
               _Disp->setPixel((_Disp->fch << 8) | _Disp->fcl);
             }
+            else
+            {
+              if (!_Disp->_transparent)
+                _Disp->setPixel((_Disp->bch << 8) | _Disp->bcl);
+            }
           }
         }
       }
-      temp += (_Disp->cfont.x_size / 8);
     }
+    temp += (_Disp->cfont.x_size / 8);
   }
+
   sbi(_Disp->P_CS, _Disp->B_CS);
   _Disp->clrXY();
 }
@@ -92,178 +77,220 @@ void TFT_ScaleFonts::printChar(byte c, int x, int y, int S)
 #if 1
 void TFT_ScaleFonts::rotateChar(byte c, int x, int y, int pos, int S, int deg)
 {
-	byte i,j,ch;
-	word temp; 
-	int newx,newy;
-	double radian;
-	radian=deg*0.0175;  
+  if (S < 1) S = 1;
 
-	cbi(_Disp->P_CS, _Disp->B_CS);
+  if ((S == 1) && (deg == 0))
+  {
+    _Disp->printChar(c, x, y);
+    return;
+  }
+  else if (S == 1)
+  {
+    _Disp->rotateChar(c, x, y, pos, deg);
+    return;
+  }
+  else if (deg == 0)
+  {
+    printChar(c, x, y, S);
+    return;
+  }
 
-	temp=((c-_Disp->cfont.offset)*((_Disp->cfont.x_size/8)*_Disp->cfont.y_size))+4;
-	for(j=0;j<_Disp->cfont.y_size*S;j+=S) 
-	{
-		for (int zz=0; zz<(_Disp->cfont.x_size/8); zz++)
-		{
-			ch=pgm_read_byte(&_Disp->cfont.font[temp+zz]); 
-			for(i=0;i<(8*S);i++)
-			{   
-			  for(int s = 0; s < S; s++)
-		      {
-				newx=x+(((i+(zz*8)+(pos*_Disp->cfont.x_size))*cos(radian))-((j+s)*sin(radian)));
-				newy=y+(((j+s)*cos(radian))+((i+(zz*8)+(pos*_Disp->cfont.x_size))*sin(radian)));
+  byte i, j, ch;
+  word temp;
+  int newx, newy;
+  double radian;
+  radian = deg * 0.0175;
 
-				_Disp->setXY(newx,newy,newx+1,newy+1);
-				
-				if((ch&(1<<(7-(i/S))))!=0)   
-				{
-					_Disp->setPixel((_Disp->fch<<8)|_Disp->fcl);
-				} 
-				else  
-				{
-					if (!_Disp->_transparent)
-						_Disp->setPixel((_Disp->bch<<8)|_Disp->bcl);
-				} 
-              }				
-			}
-		}
-		temp+=(_Disp->cfont.x_size/8);
-	}
-	sbi(_Disp->P_CS, _Disp->B_CS);
-	_Disp->clrXY();
+  cbi(_Disp->P_CS, _Disp->B_CS);
+
+  temp = ((c - _Disp->cfont.offset) * ((_Disp->cfont.x_size / 8) * _Disp->cfont.y_size)) + 4;
+  for (j = 0; j < _Disp->cfont.y_size * S; j += S)
+  {
+    for (int zz = 0; zz < (_Disp->cfont.x_size / 8)*S; zz += S)
+    {
+      ch = pgm_read_byte(&_Disp->cfont.font[temp + (zz / S)]);
+      for (i = 0; i < (8 * S); i++)
+      {
+        for (int s = 0; s < S; s++)
+        {
+          newx = x + (((i + (zz * 8) + (pos * _Disp->cfont.x_size)) * cos(radian)) - ((j + s) * sin(radian)));
+          newy = y + (((j + s) * cos(radian)) + ((i + (zz * 8) + (pos * _Disp->cfont.x_size)) * sin(radian)));
+
+          _Disp->setXY(newx, newy, newx + 1, newy + 1);
+
+          if ((ch & (1 << (7 - (i / S)))) != 0)
+          {
+            _Disp->setPixel((_Disp->fch << 8) | _Disp->fcl);
+          }
+          else
+          {
+            if (!_Disp->_transparent)
+              _Disp->setPixel((_Disp->bch << 8) | _Disp->bcl);
+          }
+        }
+      }
+    }
+    temp += (_Disp->cfont.x_size / 8);
+  }
+  sbi(_Disp->P_CS, _Disp->B_CS);
+  _Disp->clrXY();
 }
 
 void TFT_ScaleFonts::print(char *st, int x, int y, int S, int deg)
 {
-	int stl, i;
+  if (S < 1) S = 1;
 
-	stl = strlen(st);
+  if (S == 1)
+  {
+    _Disp->print(st, x, y, deg);
+    return;
+  }
 
-	if (_Disp->orient==PORTRAIT)
-	{
-	if (x==RIGHT)
-		x=(_Disp->disp_x_size+1)-(stl*_Disp->cfont.x_size);
-	if (x==CENTER)
-		x=((_Disp->disp_x_size+1)-(stl*_Disp->cfont.x_size))/2;
-	}
-	else
-	{
-	if (x==RIGHT)
-		x=(_Disp->disp_y_size+1)-(stl*_Disp->cfont.x_size);
-	if (x==CENTER)
-		x=((_Disp->disp_y_size+1)-(stl*_Disp->cfont.x_size))/2;
-	}
-    
-	for (i=0; i<stl; i++)
-		if (deg==0)
-			printChar(*st++, x + (i*(_Disp->cfont.x_size*S)), y, S);
-		else
-			rotateChar(*st++, x, y, i,S,deg);
+  int stl, i;
+
+  stl = strlen(st);
+
+  if (_Disp->orient == PORTRAIT)
+  {
+    if (x == RIGHT)
+      x = ((Bound_X2 - Bound_X)/*_Disp->disp_x_size*/ + 1) - (stl * _Disp->cfont.x_size * S);
+    if (x == CENTER)
+      x = (((Bound_X2 - Bound_X) + 1) - (stl * _Disp->cfont.x_size * S)) / 2;
+  }
+  else
+  {
+    if (x == RIGHT)
+      x = ((Bound_Y2 - Bound_Y) + 1) - (stl * _Disp->cfont.x_size * S);
+    if (x == CENTER)
+      x = (((Bound_Y2 - Bound_Y) + 1) - (stl * _Disp->cfont.x_size * S)) / 2;
+  }
+
+  for (i = 0; i < stl; i++)
+    if (deg == 0)
+      printChar(*st++, x + (i * (_Disp->cfont.x_size * S)), y, S);
+    else
+      rotateChar(*st++, x, y, i * S, S, deg);
 }
 
 void TFT_ScaleFonts::printNumI(long num, int x, int y, int length, byte S, char filler)
 {
-	char buf[25];
-	char st[27];
-	boolean neg=false;
-	int c=0, f=0;
-  
-	if (num==0)
-	{
-		if (length!=0)
-		{
-			for (c=0; c<(length-1); c++)
-				st[c]=filler;
-			st[c]=48;
-			st[c+1]=0;
-		}
-		else
-		{
-			st[0]=48;
-			st[1]=0;
-		}
-	}
-	else
-	{
-		if (num<0)
-		{
-			neg=true;
-			num=-num;
-		}
-	  
-		while (num>0)
-		{
-			buf[c]=48+(num % 10);
-			c++;
-			num=(num-(num % 10))/10;
-		}
-		buf[c]=0;
-	  
-		if (neg)
-		{
-			st[0]=45;
-		}
-	  
-		if (length>(c+neg))
-		{
-			for (int i=0; i<(length-c-neg); i++)
-			{
-				st[i+neg]=filler;
-				f++;
-			}
-		}
+  if (S < 1) S = 1;
 
-		for (int i=0; i<c; i++)
-		{
-			st[i+neg+f]=buf[c-i-1];
-		}
-		st[c+neg+f]=0;
+  if (S == 1)
+  {
+    _Disp->printNumI(num, x, y, length, filler);
+    return;
+  }
 
-	}
+  char buf[25];
+  char st[27];
+  boolean neg = false;
+  int c = 0, f = 0;
 
-	print(st,x,y, S);
+  if (num == 0)
+  {
+    if (length != 0)
+    {
+      for (c = 0; c < (length - 1); c++)
+        st[c] = filler;
+      st[c] = 48;
+      st[c + 1] = 0;
+    }
+    else
+    {
+      st[0] = 48;
+      st[1] = 0;
+    }
+  }
+  else
+  {
+    if (num < 0)
+    {
+      neg = true;
+      num = -num;
+    }
+
+    while (num > 0)
+    {
+      buf[c] = 48 + (num % 10);
+      c++;
+      num = (num - (num % 10)) / 10;
+    }
+    buf[c] = 0;
+
+    if (neg)
+    {
+      st[0] = 45;
+    }
+
+    if (length > (c + neg))
+    {
+      for (int i = 0; i < (length - c - neg); i++)
+      {
+        st[i + neg] = filler;
+        f++;
+      }
+    }
+
+    for (int i = 0; i < c; i++)
+    {
+      st[i + neg + f] = buf[c - i - 1];
+    }
+    st[c + neg + f] = 0;
+
+  }
+
+  print(st, x, y, S);
 }
 
 void TFT_ScaleFonts::printNumF(double num, byte dec, int x, int y, char divider, int length, byte S, char filler)
 {
-	char st[27];
-	boolean neg=false;
+  if (S < 1) S = 1;
 
-	if (dec<1)
-		dec=1;
-	else if (dec>5)
-		dec=5;
+  if (S == 1)
+  {
+    _Disp->printNumF(num, dec, x, y, divider, length, filler);
+    return;
+  }
 
-	if (num<0)
-		neg = true;
+  char st[27];
+  boolean neg = false;
 
-	ftos(num, length, dec, st);
+  if (dec < 1)
+    dec = 1;
+  else if (dec > 5)
+    dec = 5;
 
-	if (divider != '.')
-	{
-		for (int i=0; i<sizeof(st); i++)
-			if (st[i]=='.')
-				st[i]=divider;
-	}
+  if (num < 0)
+    neg = true;
 
-	if (filler != ' ')
-	{
-		if (neg)
-		{
-			st[0]='-';
-			for (int i=1; i<sizeof(st); i++)
-				if ((st[i]==' ') || (st[i]=='-'))
-					st[i]=filler;
-		}
-		else
-		{
-			for (int i=0; i<sizeof(st); i++)
-				if (st[i]==' ')
-					st[i]=filler;
-		}
-	}
+  ftos(num, length, dec, st);
 
-	print(st,x,y, S);
+  if (divider != '.')
+  {
+    for (int i = 0; i < sizeof(st); i++)
+      if (st[i] == '.')
+        st[i] = divider;
+  }
+
+  if (filler != ' ')
+  {
+    if (neg)
+    {
+      st[0] = '-';
+      for (int i = 1; i < sizeof(st); i++)
+        if ((st[i] == ' ') || (st[i] == '-'))
+          st[i] = filler;
+    }
+    else
+    {
+      for (int i = 0; i < sizeof(st); i++)
+        if (st[i] == ' ')
+          st[i] = filler;
+    }
+  }
+
+  print(st, x, y, S);
 }
 
 char * TFT_ScaleFonts::ftos(float data, byte W, byte D, char * buf)
--- TFT_ScaleFonts.h
+++ TFT_ScaleFonts.h
@@ -1,36 +1,15 @@
 #ifndef TFT_ScaleFonts_h
 #define TFT_ScaleFonts_h
 
-/*
-The MIT License (MIT)
-
-Copyright (c) 2016 Andrew Mascolo Jr
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-
 #include <UTFT.h>
+#define PORTRAIT 0
+#define LANDSCAPE 1
 
 class TFT_ScaleFonts 
 {
   public:
-		TFT_ScaleFonts(UTFT * disp): _Disp(disp){}
+		TFT_ScaleFonts(UTFT * disp): _Disp(disp){ TextBounds(0,0,_Disp->disp_x_size,_Disp->disp_y_size); }
+		void	TextBounds(int x, int y, int x2, int y2);
     	void	print(char *st, int x, int y, int S = 1, int deg=0);
 		void	printNumI(long num, int x, int y, int length=0, byte S = 1, char filler=' ');
 		void	printNumF(double num, byte dec, int x, int y, char divider='.', int length=0, byte S = 1, char filler=' ');
@@ -39,6 +18,7 @@ class TFT_ScaleFonts
   private:
 		char * ftos(float data, byte W, byte D, char * buf);
 		unsigned long Pow(long V, byte shift);
+		int Bound_X, Bound_Y, Bound_X2, Bound_Y2;
   protected:	
     UTFT * _Disp;
 };
